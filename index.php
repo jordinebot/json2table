b3788637bab4d6acffbcafdfb49edaa2d1a08cfb
Jordi Nebot <llengot@gmail.com>
2012-07-16 13:40:23 +0200

    Working example

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-	$json11_url = "https://graph.facebook.com/10151021544704634/comments?value=1&redirect=1&limit=1000&offset=0&__after_id=10151021544704634_23455194";
+	$json11_url = "https://graph.facebook.com/10151021544704634/comments?value=1&redirect=1&limit=1000";
 
 	// Initializing curl
 	$ch = curl_init( $json11_url );
@@ -16,6 +16,9 @@
 	 
 	// Getting results
 	$result =  curl_exec($ch); // Getting jSON result string
+	
+	// Converting JSON to Array
+	$json_array = json_decode($result, true);
 
 
 
@@ -32,12 +35,34 @@
 </head>
 
 <body>
+<h1>Dia 11</h1>
+<table>
+<tr>
+	<th>Nom</th>
+    <th>ID</th>
+    <th>Missatge</th>
+    <th>Data</th>
+
+</tr>
 <?php
 
-print_r($result);
+	foreach ($json_array['data'] as $item) {
+		
+		echo "<tr>";
+		
+		echo "<td>" . $item['from']['name'] . "</td>";
+		echo "<td>" . $item['from']['id'] . "</td>";
+		echo "<td>" . $item['message'] . "</td>";
+		echo "<td>" . $item['created_time'] . "</td>";
+		
+		
+		echo "</tr>";
+		
+	
+	}
 
 
 ?>
-
+</table>
 </body>
 </html>
