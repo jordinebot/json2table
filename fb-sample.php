3a1c72acae63e77b42dfc99839de02c0cc2696d6
Jordi Nebot <llengot@gmail.com>
2012-07-17 11:08:04 +0300

    Purged blank lines

--- fb-sample.php
+++ fb-sample.php
@@ -1,7 +1,5 @@
 <?php 
-
 	/* https://graph.facebook.com/comments/?ids={YOUR_URL} */
-
 	$json11_url = "https://graph.facebook.com/10151021544704634/comments?value=1&redirect=1&limit=1000";
 	$json12_url = "https://graph.facebook.com/10151076238995519/comments?value=1&redirect=1&limit=1000";
 	$json13_url = "https://graph.facebook.com/10150956342733212/comments?value=1&redirect=1&limit=1000";
@@ -9,18 +7,14 @@
 
 	require_once('functions.php');
 	
-	$json_array = get_json_array($json16_url);
-	
+	$json_array = get_json_array($json16_url);	
 ?>
-
-
 <!doctype html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>JSON 2 Table</title>
 </head>
-
 <body>
 <table>
 <tr>
@@ -31,24 +25,15 @@
 
 </tr>
 <?php
-
-
 	foreach ($json_array['data'] as $item) {
 		
-		echo "<tr>";
-		
+		echo "<tr>";		
 		echo "<td>" . $item['from']['name'] . "</td>";
 		echo "<td>" . $item['from']['id'] . "</td>";
 		echo "<td>" . $item['message'] . "</td>";
 		echo "<td>" . $item['created_time'] . "</td>";
-		
-		
 		echo "</tr>";
-		
-	
 	}
-
-
 ?>
 </table>
 </body>
